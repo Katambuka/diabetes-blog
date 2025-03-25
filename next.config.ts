6affdb70a4963a42f78b7c290ed92b5b546b98e1
Katambuka <kan22001@byui.edu>
2025-03-25 14:06:26 +0400

    routes updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-  output: 'export', 
-  trailingSlash: true, 
-  images: {
-    unoptimized: true, 
-  },
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/diabetes-blog', 
+  images: { unoptimized: true }
 
 };
 
